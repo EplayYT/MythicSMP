cf112b2278ae463499fc28970cdf308ff9887fa3
EplayYT <ps222iki@gmail.com>
2025-11-13 13:53:43 +0300

    Create MythicSMP.java

---
+++ src/com/eplayyt/mythicsmp/MythicSMP.java
@@ -0,0 +1,18 @@
+package com.eplayyt.mythicsmp;
+
+import com.eplayyt.mythicsmp.commands.MythCreateCommand;
+import com.eplayyt.mythicsmp.commands.MythInfoCommand;
+import com.eplayyt.mythicsmp.data.PlayerMythManager;
+
+public class MythicSMP {
+
+    public void onEnable() {
+        System.out.println("[MythicSMP] Plugin aktif!");
+        // Komut kayıtları burada yapılacak
+    }
+
+    public void onDisable() {
+        System.out.println("[MythicSMP] Plugin kapandı!");
+    }
+
+}
