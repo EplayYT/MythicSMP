1a94d306c0f8dc76a872cd34bc39acc8f1f33802
EplayYT <ps222iki@gmail.com>
2025-11-13 14:01:01 +0300

    Create MythInfoCommand.java

---
+++ src/com/eplayyt/mythicsmp/commands/MythInfoCommand.java
@@ -0,0 +1,10 @@
+package com.eplayyt.mythicsmp.commands;
+
+public class MythInfoCommand {
+
+    public void execute(String playerName) {
+        System.out.println(playerName + " efsanesi gösteriliyor.");
+        // Burada PlayerMythManager’dan veri çekebilirsin acıklama2
+    }
+
+}
