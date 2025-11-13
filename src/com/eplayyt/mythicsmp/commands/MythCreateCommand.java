f934cec66458c107d6a97718a37522f29b9b5989
EplayYT <ps222iki@gmail.com>
2025-11-13 13:59:02 +0300

    Create MythCreateCommand.java

---
+++ src/com/eplayyt/mythicsmp/commands/MythCreateCommand.java
@@ -0,0 +1,10 @@
+package com.eplayyt.mythicsmp.commands;
+
+public class MythCreateCommand {
+
+    public void execute(String playerName, String myth) {
+        System.out.println(playerName + " efsanesini oluşturdu: " + myth);
+        // Burada PlayerMythManager ile veri kaydedebilirsin
+    }
+
+}
