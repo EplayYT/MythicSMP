69050cdb5ddce1097f1607e50a2f998faea9e996
EplayYT <ps222iki@gmail.com>
2025-11-23 10:41:40 +0300

    Clean up MythInfoCommand by removing comment
    Removed commented code for clarity.

--- src/com/eplayyt/mythicsmp/commands/MythInfoCommand.java
+++ src/com/eplayyt/mythicsmp/commands/MythInfoCommand.java
@@ -4,7 +4,7 @@ public class MythInfoCommand {
 
     public void execute(String playerName) {
         System.out.println(playerName + " efsanesi gösteriliyor.");
-        // Burada PlayerMythManager’dan veri çekebilirsin acıklama2
+        
     }
 
 }
